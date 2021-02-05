1064b77975f90f1d94fc52c3c831362ab10be78d
Malkeet11 <52320147+Malkeet11@users.noreply.github.com>
2021-02-06 01:22:27 +0530

    Day 1 Assignment
    Introduction, Intro to Python, Python List, Time & Space Complexity

---
+++ Day 1 Assignment/Assignment-2.py
@@ -0,0 +1,15 @@
+'''
+Python-Handler
+Author : Malkeet Singh
+Assignment - 2
+Date : 2021-02-05
+'''
+
+# Taking User Input Base in Variable b
+b = float(input('Enter the Base : '))
+
+# Taking User Input Base in Variable b
+e = float(input('Enter the Exponent : '))
+
+# Base b raised to power of Exponent e is presented as b**e
+print (b**e)
